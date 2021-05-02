e0b6c6da28bd06ff6e19da645ccccb9e3f80e4bc
iMasanari <14dd023@gmail.com>
2021-05-02 10:30:49 +0900

    workerの出力パス設定を修正

--- next.config.js
+++ next.config.js
@@ -2,12 +2,16 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  webpack(config) {
+  webpack(config, { isServer, dev }) {
     config.resolve.fallback = {
       fs: false,
       path: false,
       crypto: false,
     }
+
+    config.output.chunkFilename = isServer
+      ? `${dev ? '[name]' : '[name].[fullhash]'}.js`
+      : `static/chunks/${dev ? '[name]' : '[name].[fullhash]'}.js`
 
     return config
   },
