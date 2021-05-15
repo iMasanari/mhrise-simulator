549079e173b6e28b00c8ac1d0f6f3eeef23f8c89
iMasanari <14dd023@gmail.com>
2021-05-15 17:31:47 +0900

    シミュページの初期データを変更

--- src/api/skills.ts
+++ src/api/skills.ts
@@ -1,17 +1,3 @@
-import { SkillSystem } from '../domain/skill'
-
-export const getSkillSystems = async () => {
-  const skills = await import('../../generated/skills.json').then(v => v.default)
-
-  const skillSystems = skills.map(({ name, category, details }): SkillSystem => ({
-    name,
-    category,
-    items: details.map(v => v.point),
-  }))
-
-  return skillSystems
-}
-
 export const getSkills = async () => {
   const skills = await import('../../generated/skills.json').then(v => v.default)
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,22 @@
 import Container from '@material-ui/core/Container'
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import * as React from 'react'
-import { getSkillSystems } from '../api/skills'
+import React from 'react'
+import skills from '../../generated/skills.json'
 import Simulator from '../components/templates/Simulator'
 import { SkillSystem } from '../domain/skill'
 
-interface Props {
-  skills: SkillSystem[]
-}
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const skills = await getSkillSystems()
-
-  return {
-    props: { skills },
-  }
-}
-
-export default function TopPage({ skills }: Props) {
+export default function TopPage() {
+  const skillList = skills.map(({ name, category, details }): SkillSystem => ({
+    name,
+    category,
+    items: details.map(v => v.point),
+  }))
   return (
     <Container maxWidth="md">
       <Head>
         <title>MHRise スキルシミュ</title>
       </Head>
-      <Simulator skills={skills} />
+      <Simulator skills={skillList} />
     </Container>
   )
 }
