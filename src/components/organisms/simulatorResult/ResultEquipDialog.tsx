5089904d9f071d315ac247e9fe189f56a6335a2e
iMasanari <14dd023@gmail.com>
2021-06-26 23:00:09 +0900

    装備結果表示を修正

--- src/components/organisms/simulatorResult/ResultEquipDialog.tsx
+++ src/components/organisms/simulatorResult/ResultEquipDialog.tsx
@@ -1,8 +1,9 @@
-import { Button, Dialog, DialogContent, DialogTitle } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Box, Typography } from '@material-ui/core'
 import { Link as LinkIcon } from '@material-ui/icons'
 import React, { useState } from 'react'
 import { Equip } from '../../../domain/equips'
 import ResultEquip from '../../molecules/ResultEquip'
+import ResultSkills from '../../molecules/ResultSkills'
 import ShareDialog from './ShareDialog'
 
 interface Props {
@@ -19,10 +20,16 @@ export default function SimulatorResultRow({ equip, open, onClose }: Props) {
       <DialogTitle>装備詳細</DialogTitle>
       <DialogContent>
         <ResultEquip equip={equip} size="small" />
+        <Box mt={2}>
+          <Typography>発動スキル</Typography>
+          <ResultSkills equip={equip} />
+        </Box>
+      </DialogContent>
+      <DialogActions>
         <Button startIcon={<LinkIcon />} fullWidth sx={{ mt: 1 }} onClick={() => setShareDialogOpen(true)}>
           {'結果を共有する'}
         </Button>
-      </DialogContent>
+      </DialogActions>
       {isShareDialogOpen && <ShareDialog open={isShareDialogOpen} onClose={() => setShareDialogOpen(false)} equip={equip} />}
     </Dialog>
   )
