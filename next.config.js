6c91b66287e35bb32a721723f040deacb91411b0
iMasanari <14dd023@gmail.com>
2021-06-28 23:57:20 +0900

    update Next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   webpack(config) {
-    // https://github.com/vercel/next.js/issues/26152#issuecomment-862346528
-    config.optimization.splitChunks = false
-
     config.resolve.fallback = {
       ...(config.resolve.fallback || {}),
       fs: false,
