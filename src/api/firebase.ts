7bd69d428fa4d84884e83e3092a8c61dc1195619
iMasanari <14dd023@gmail.com>
2023-02-05 19:02:24 +0900

    firestoreでrest apiを使用するように変更

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,17 +1,12 @@
-import admin from 'firebase-admin'
+import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app'
+import { initializeFirestore } from 'firebase-admin/firestore'
 
-const initializeApp = () => {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    }),
-  })
-}
+const app = getApps().length ? getApp() : initializeApp({
+  credential: cert({
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  }),
+})
 
-admin.apps.length ? admin.app() : initializeApp()
-
-export const firestore = admin.firestore()
-
-export const FieldValue = admin.firestore.FieldValue
+export const firestore = initializeFirestore(app, { preferRest: true })
